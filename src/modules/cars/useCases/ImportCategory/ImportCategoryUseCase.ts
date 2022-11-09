a1cc0b3199c4dd7003bef310035093f553d2c4ae
abner <abnersantos@zaite.comm.br>
2022-11-09 14:50:06 -0300

    commit final

--- src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -11,24 +11,42 @@ class ImportCategoryUseCase{
     constructor(private categoriesRepository: ICategoryRepository) {}
 
 
-    loadCategories(file: Express.Multer.File): IImportCategory[]{
-        const stream = fs.createReadStream(file.path);
-        const categories: IImportCategory[] = [];
-        const parseFile = parse();
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            const parseFile = parse();
 
-        stream.pipe(parseFile);
-        parseFile.on('data', async (line) => {
-            const [name, description] = line;
-            categories.push({
-                name, description,
-            });
-        });
-        return categories;
+            stream.pipe(parseFile);
+            parseFile.on('data', async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name, description,
+                });
+              })
+              .on('end', () => {
+                fs.promises.unlink(file.path)
+                resolve(categories);
+              })
+              .on("error", (erro) => {
+                reject(erro);
+              });
+            })
     }
 
-    execute(file: Express.Multer.File): void {
-       const categories = this.loadCategories(file);
-       console.log(categories);
+    async execute(file: Express.Multer.File): Promise<void> {
+       const categories = await this.loadCategories(file);
+       categories.map(async (category) => {
+        const { name, description } = category;
+        const existCategory = await this.categoriesRepository.findByName(name);
+
+        if(!existCategory){
+            await this.categoriesRepository.create({
+                name,
+                description,
+            });
+        }
+       })
     }
 }
 
